f0132ab8b10d6c025b43e30373bcc7e144daaf05
muntinova <assyl.toka@gmail.com>
2021-10-11 02:24:48 -0500

    added menu

--- app/src/main/java/com/example/mycvhw/MainActivity.kt
+++ app/src/main/java/com/example/mycvhw/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mycvhw
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -12,9 +13,11 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.mycvhw.ui.main.SectionsPagerAdapter
 import com.example.mycvhw.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home_freg.*
 
 class MainActivity : AppCompatActivity() {
@@ -36,7 +39,47 @@ class MainActivity : AppCompatActivity() {
 //        val adapter = ViewPagerAdapter(supportFragmentManager)
 
 
+        topAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+        }
 
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.telegram -> {
+                    // Handle favorite icon press
+                    true
+                }
+                R.id.linkedin -> {
+                    // Handle search icon press
+                    true
+                }
+                R.id.whatsapp -> {
+                    // Handle search icon press
+                    true
+                }
+                R.id.gmail -> {
+                    // Handle more item (inside overflow menu) press
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        // Code to get the title and icon on the option overflow
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item:MenuItem):Boolean {
+        Toast.makeText(
+            applicationContext,
+            item.title.toString(),
+            Toast.LENGTH_LONG).show()
+        return super.onOptionsItemSelected(item)
     }
 
     fun addNewSkill(view: View) {
@@ -46,4 +89,6 @@ class MainActivity : AppCompatActivity() {
 //            val mAlertDialog = mBuilder.show()
 
     }
+
+
 }
